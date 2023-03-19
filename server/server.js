7cf12bdd69264a2dad609462c0843340738db6a4
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-03-19 16:58:00 +0530

    added the codex da-vinci-002 inplace of text-davinci-03

--- server/server.js
+++ server/server.js
@@ -24,15 +24,27 @@ app.get('/',async (req,res)=>{
 app.post('/', async(req,res) =>{
    try {
     const prompt = req.body.prompt;
-
+    /////
+    
+    ///
+// initial version of coding solutions ai
+    // const response = await openai.createCompletion({
+    //     model:"text-davinci-003",
+    //     prompt:`${prompt}`,
+    //     temperature:0,
+    //     max_tokens: 30000,
+    //     top_p:1,
+    //     frequency_penalty:0.5,
+    //     presence_penalty:0,
+    // });
     const response = await openai.createCompletion({
-        model:"text-davinci-003",
+        model:"code-davinci-002",
         prompt:`${prompt}`,
         temperature:0,
-        max_tokens: 30000,
+        max_tokens:3000,
         top_p:1,
         frequency_penalty:0.5,
-        presence_penalty:0,
+        presence_penalty:0
     });
     res.status(200).send({
         bot:response.data.choices[0].text
