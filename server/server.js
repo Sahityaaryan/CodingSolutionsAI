2fb1069a6546ef89fa71a087dafd11d570a0d54e
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-03-19 17:05:02 +0530

    changed the limit of no. of tokens to 256

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.post('/', async(req,res) =>{
         model:"code-davinci-002",
         prompt:`${prompt}`,
         temperature:0,
-        max_tokens:3000,
+        max_tokens:256,
         top_p:1,
         frequency_penalty:0.5,
         presence_penalty:0
